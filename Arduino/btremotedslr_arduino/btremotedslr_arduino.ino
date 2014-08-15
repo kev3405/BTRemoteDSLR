e9941f20d65d1da9efb05d1d760b910692b07322
kevin <kevin.e@gmx.de>
2014-08-15 10:40:06 +0200

    Arduino angepasst + neue Version (einfacher)

--- Arduino/btremotedslr_arduino/btremotedslr_arduino.ino
+++ Arduino/btremotedslr_arduino/btremotedslr_arduino.ino
@@ -1,9 +1,68 @@
-void setup() {
-  // put your setup code here, to run once:
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Definitionen für physikalischen Zugriff
+int focus = 9;
+int shoot = 8;
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Variablen und Konstanten zum Informationsaustausch
+
+uint8_t vReceive[3];
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// interne Bearbeitung von Aufträgen
+uint8_t time_interval; //Zeitintervall zwischen 2 Bildern
+uint16_t pictureTaken; // Anzahl der durchgeführten Auslösungen
+uint16_t pictureCount; // Gesamtanzahl der durch zu führenden Auslösungen
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+void setup() {
+    //Initialisierung von Softwarestrukturen
+    time_interval = 0;
+    pictureTaken = 0;
+    pictureCount = 0;
+  
+    //Schnittstelle zur DSLR
+    pinMode(focus, OUTPUT);
+    pinMode(shoot, OUTPUT);
+    digitalWrite(focus, LOW);
+    digitalWrite(shoot, LOW);
+  
+    //Schnittstelle zum Bluetooth Modul
+    Serial.begin(9600);    
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //sind 3 Bytes empfangen?
+  while(Serial.available() < 3);
+  
+  //Daten lesen
+  vReceive[0] = Serial.read();  
+  vReceive[1] = Serial.read();
+  vReceive[2] = Serial.read();
 
+  time_interval = vReceive[0];
+  pictureCount = (((uint16_t) vReceive[1]) << 8) | ((uint16_t) vReceive[2]);
+  
+  Serial.print("Time Intervall: "); Serial.println(time_interval, DEC);
+  Serial.print("Picture Count: "); Serial.println(pictureCount, DEC);
+  
+  //antworten!
+  Serial.print(0xFF); 
+
+  //Daten verarbeiten ... in dieser Zeit werden keine neuen Daten empfangen!!
+  for(uint16_t i = 0; i < pictureCount; i++)
+  {
+    Serial.print("shoot ... "); Serial.println(i, DEC);
+    digitalWrite(shoot, HIGH);
+    delay(500);
+    digitalWrite(shoot, LOW);
+    delay(500);
+    for(int j = 0; j < time_interval - 1 ; j++)
+    {
+      delay(1000);
+    }
+  }  
 }
